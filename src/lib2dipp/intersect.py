a92ffed01b49700f0e56e0f930c8274dc26c6c04
Emerson MX <emersonmx@gmail.com>
2013-10-18 03:28:06 -0300

    Implementação do calculo para encontrar os pontos.
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

--- src/lib2dipp/intersect.py
+++ src/lib2dipp/intersect.py
@@ -18,11 +18,8 @@
 from math import sqrt
 from shape import *
 
-def sgn(x):
-    if x < 0:
-        return -1
-    else:
-        return 1
+def point_in_rect(point, rect):
+    return (rect[0] <= point.x <= rect[2]) and (rect[1] <= point.y <= rect[3])
 
 def lines(line1, line2):
     """See p.199-201 of Graphic Gems 3.
@@ -47,32 +44,62 @@ def lines(line1, line2):
 
     return None
 
-def line_arc(line, arc):
+def _calculate_points(line, arc):
     x1, y1 = line.begin
     x2, y2 = line.end
+    cx, cy = arc.centre_point
 
     dx = x2 - x1
     dy = y2 - y1
-    dr = sqrt((dx**2) + (dy**2))
-    dr2 = (dr**2)
-    D = (x1 * y2) - (x2 * y1)
+    a = dx**2 + dy**2
+    b = 2 * (dx * (x1 - cx) + dy * (y1 - cy))
+    c = cx**2 + cy**2
+    c += x1**2 + y1**2
+    c -= 2 * (cx * x1 + cy * y1)
+    c -= arc.radius**2
 
-    delta = (arc.radius**2) * dr2 - (D**2)
+    delta = b**2 - 4 * a * c
     if delta < 0:
         return None
+    elif delta == 0:
+        result = (-b + sqrt(delta)) / (2 * a)
+        x_ = x1 + result * dx
+        y_ = y1 + result * dy
 
-    x_ = (D * dy + sgn(dy) * dx * sqrt(delta)) / dr2
-    x__ = (D * dy - sgn(dy) * dx * sqrt(delta)) / dr2
-    y_ = (-D * dx + abs(dy) * sqrt(delta)) / dr2
-    y__ = (-D * dx - abs(dy) * sqrt(delta)) / dr2
+        return Point(x_, y_)
+    else:
+        result = (-b + sqrt(delta)) / (2 * a)
+        x_ = x1 + result * dx
+        y_ = y1 + result * dy
+        result = (-b - sqrt(delta)) / (2 * a)
+        x__ = x1 + result * dx
+        y__ = y1 + result * dy
 
-    return (Point(x_, y_), Point(x__, y__))
+        return Point(x_, y_), Point(x__, y__)
+
+def line_arc(line, arc):
+    result = None
+    points = _calculate_points(line, arc)
+    if points:
+        aabb = line.bounds()
+        if isinstance(points, tuple):
+            result_list = []
+            for point in points:
+                if point_in_rect(point, aabb):
+                    result_list.append(point)
+
+            result = result_list
+        else:
+            if point_in_rect(points, aabb):
+                result = points
+
+    return result
 
 def arcs(arc1, arc2):
     pass
 
 if __name__ == "__main__":
-    l = Line(begin=(4., 0.), end=(0., 4.))
+    l = Line(begin=(0., 4.), end=(4., 0.))
     a = Arc(centre_point=(2., 2.), radius=2.,
             start_angle=0., offset_angle=2 * math.pi)
 
