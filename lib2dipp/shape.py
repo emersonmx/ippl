0fd8fda5bbd8d15c99a5cc777da388c645e9b830
Emerson MX <emersonmx@gmail.com>
2013-10-28 00:14:29 -0300

    Movendo tudo relacionado a shapes para o módulo shape.
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

--- lib2dipp/shape.py
+++ lib2dipp/shape.py
@@ -17,7 +17,159 @@
 
 import math
 
-import util
+def wrap_2pi(angle):
+    return angle % (math.pi * 2)
+
+def wrap_360(angle):
+    return angle % 360
+
+def angle_in_range(angle, start, end):
+    """Checks whether an angle is between start and end.
+
+    params:
+        angle in radians in range [0, math.pi*2)
+        start angle in radians in range [0, math.pi*2)
+        end angle in range in range [0, math.pi*2)
+    """
+
+    if wrap_2pi(start) >= wrap_2pi(end):
+        if start <= angle <= (math.pi * 2) or 0.0 <= angle <= end:
+            return True
+        else:
+            return False
+
+    return start <= angle <= end
+
+def calculate_line_circle_points(line, circle):
+    x1, y1 = line.begin
+    x2, y2 = line.end
+    cx, cy = circle.centre_point
+
+    dx = x2 - x1
+    dy = y2 - y1
+    a = dx**2 + dy**2
+    b = 2 * (dx * (x1 - cx) + dy * (y1 - cy))
+    c = cx**2 + cy**2
+    c += x1**2 + y1**2
+    c -= 2 * (cx * x1 + cy * y1)
+    c -= circle.radius**2
+
+    delta = b**2 - 4 * a * c
+    if delta < 0:
+        return []
+    else:
+        result = (-b + math.sqrt(delta)) / (2 * a)
+        x_ = x1 + result * dx
+        y_ = y1 + result * dy
+        if delta == 0:
+            return [Point(x_, y_)]
+
+        result = (-b - math.sqrt(delta)) / (2 * a)
+        x__ = x1 + result * dx
+        y__ = y1 + result * dy
+
+        return [Point(x_, y_), Point(x__, y__)]
+
+def calculate_circles_points(circle1, circle2, distance=None):
+    p1 = circle1.centre_point
+    r1 = circle1.radius
+    p2 = circle2.centre_point
+    r2 = circle2.radius
+
+    if not distance:
+        distance = p1.distance(p2)
+
+    a = (r1**2 - r2**2 + distance**2) / (2 * distance)
+    h = math.sqrt(r1**2 - a**2)
+    s = a / distance
+    p3 = Point(p1.x + s * (p2.x - p1.x), p1.y + s * (p2.y - p1.y))
+
+    x3 = p3.x + h * (p2.y - p1.y) / distance
+    y3 = p3.y - h * (p2.x - p1.x) / distance
+    x4 = p3.x - h * (p2.y - p1.y) / distance
+    y4 = p3.y + h * (p2.x - p1.x) / distance
+
+    return [Point(x3, y3), Point(x4, y4)]
+
+def calculate_perpendicular_line(line, point):
+    begin, end = line.begin, line.end
+    x1, y1, x2, y2 = begin.x, begin.y, end.x, end.y
+    x3, y3 = point.x, point.y
+
+    k = (((y2 - y1) * (x3 - x1) - (x2 - x1) * (y3 - y1)) /
+         ((y2 - y1)**2 + (x2 - x1)**2))
+    x4 = x3 - k * (y2 - y1)
+    y4 = y3 + k * (x2 - x1)
+
+    return Point(x4, y4)
+
+def point_in_rect(point, rect):
+    return (rect[0] <= point.x <= rect[2]) and (rect[1] <= point.y <= rect[3])
+
+def lines(line1, line2):
+    """See p.199-201 of Graphic Gems 3."""
+
+    p1, p2, p3, p4 = line1.begin, line1.end, line2.begin, line2.end
+
+    a = Point(p2.x - p1.x, p2.y - p1.y)
+    b = Point(p3.x - p4.x, p3.y - p4.y)
+    c = Point(p1.x - p3.x, p1.y - p3.y)
+
+    denominator = (a.y * b.x) - (a.x * b.y)
+    collinear = (denominator == 0)
+    if collinear:
+        # Return intersection line, else None
+        return None
+
+    alpha = ((b.y * c.x) - (b.x * c.y)) / denominator
+    beta = 0
+    if 0 <= alpha <= 1:
+        beta = ((a.x * c.y) - (a.y * c.x)) / denominator
+        if 0 <= beta <= 1:
+            return Point(p1.x + alpha * (p2.x - p1.x),
+                         p1.y + alpha * (p2.y - p1.y))
+
+    return None
+
+def line_arc(line, arc):
+    result = []
+    points = calculate_line_circle_points(line, arc)
+    if points:
+        aabb = line.bounds()
+        for point in points:
+            if point_in_rect(point, aabb):
+                angle = wrap_2pi(math.atan2(point.y - arc.centre_point.y,
+                                            point.x - arc.centre_point.x))
+                start = arc.start_angle
+                end = arc.offset_angle
+                if angle_in_range(angle, start, end):
+                    result.append(point)
+
+    return result
+
+def arcs(arc1, arc2):
+    result = []
+    p1 = arc1.centre_point
+    p2 = arc2.centre_point
+
+    distance = p1.distance(p2)
+    if (arc2.radius - arc1.radius) < distance < (arc1.radius + arc2.radius):
+        points = calculate_circles_points(arc1, arc2, distance)
+        for point in points:
+            angle1 = wrap_2pi(math.atan2(point.y - p1.y, point.x - p1.x))
+            start1 = arc1.start_angle
+            end1 = arc1.offset_angle
+            angle2 = wrap_2pi(math.atan2(point.y - p2.y, point.x - p2.x))
+            start2 = arc2.start_angle
+            end2 = arc2.offset_angle
+
+            if (angle_in_range(angle1, start1, end1) and
+                    angle_in_range(angle2, start2, end2)):
+
+                result.append(point)
+
+    # Return points, else return circle
+    return result
 
 
 class Object(object):
@@ -134,10 +286,9 @@ class Arc(Line):
         super(Arc, self).__init__(**kwargs)
 
         self.centre_point = kwargs.get("centre_point", Point())
-        self._radius = util.wrap_2pi(float(kwargs.get("radius", 1.0)))
-        self._start_angle = util.wrap_2pi(float(kwargs.get("start_angle", 0.0)))
-        self._offset_angle = util.wrap_2pi(
-            float(kwargs.get("offset_angle", 0.0)))
+        self._radius = wrap_2pi(float(kwargs.get("radius", 1.0)))
+        self._start_angle = wrap_2pi(float(kwargs.get("start_angle", 0.0)))
+        self._offset_angle = wrap_2pi(float(kwargs.get("offset_angle", 0.0)))
 
     @property
     def radius(self):
@@ -153,7 +304,7 @@ class Arc(Line):
 
     @start_angle.setter
     def start_angle(self, value):
-        self._start_angle = util.wrap_2pi(float(value))
+        self._start_angle = wrap_2pi(float(value))
 
     @property
     def offset_angle(self):
@@ -161,7 +312,7 @@ class Arc(Line):
 
     @offset_angle.setter
     def offset_angle(self, value):
-        self._offset_angle = util.wrap_2pi(float(value))
+        self._offset_angle = wrap_2pi(float(value))
 
     def calculate_ends(self):
         self.begin.x = (self.centre_point.x +
@@ -180,20 +331,20 @@ class Arc(Line):
         end = math.degrees(self.offset_angle)
         minimum_x, maximum_x, minimum_y, maximum_y = (0, 0, 0, 0)
 
-        if util.wrap_360(start) >= util.wrap_360(end):
+        if wrap_360(start) >= wrap_360(end):
             maximum_x = self.centre_point.x + self.radius
         else:
             maximum_x = max(self.begin.x, self.end.x)
-        if util.wrap_360(start - 90) >= util.wrap_360(end - 90):
+        if wrap_360(start - 90) >= wrap_360(end - 90):
             maximum_y = self.centre_point.y + self.radius
         else:
             maximum_y = max(self.begin.y, self.end.y)
 
-        if util.wrap_360(start - 180) >= util.wrap_360(end - 180):
+        if wrap_360(start - 180) >= wrap_360(end - 180):
             minimum_x = self.centre_point.x - self.radius
         else:
             minimum_x = min(self.begin.x, self.end.x)
-        if util.wrap_360(start - 270) >= util.wrap_360(end - 270):
+        if wrap_360(start - 270) >= wrap_360(end - 270):
             minimum_y = self.centre_point.y - self.radius
         else:
             minimum_y = min(self.begin.y, self.end.y)
@@ -281,4 +432,15 @@ if __name__ == "__main__":
     s.outer_loop.append(Line(begin=(1, 1), end=(0, 1)))
     s.outer_loop.append(Line(begin=(0, 1), end=(0, 0)))
     print s
+    p = Point(0, 1)
+    pl = Line(begin=(1, 1), end=(5, 5))
+    l = Line(begin=(0, 2), end=(4, 2))
+    a = Arc(centre_point=Point(2, 2), radius=2,
+            start_angle=0, offset_angle=0)
+    b = Arc(centre_point=Point(5, 2), radius=2,
+            start_angle=0, offset_angle=0)
+
+    print "Line-Arc: {}".format(line_arc(l, a))
+    print "Arcs: {}".format(arcs(a, b))
+    print "Perpendicular: {}".format(calculate_perpendicular_line(pl, p))
 
