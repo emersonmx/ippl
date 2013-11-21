ed4df8795cebe06eb240afac796231ddb769d38b
Emerson MX <emersonmx@gmail.com>
2013-11-21 10:09:21 -0300

    Adicionando licença para o script de teste.
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

--- lib2dipp/test/reader.py
+++ lib2dipp/test/reader.py
@@ -1,3 +1,22 @@
+#
+# Copyright (C) 2013 Emerson Max de Medeiros Silva
+#
+# This file is part of lib2dipp.
+#
+# lib2dipp is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# lib2dipp is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with lib2dipp.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 import ast
 import json
 import re
