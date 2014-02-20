a455b5e041d84c8eef4353eee2cadb6f5abf6ed1
Emerson MX <emersonmx@gmail.com>
2014-02-20 15:00:28 -0300

    Alterando a configuração do script.
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

---
+++ profile_tests.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+default="-p 20 -e 1000 -c 1.0 -E 0.1 -j 5"
+
+# profile 7
+basename="profile7"
+file="data/blf/$basename"
+out="-o $basename"
+config="$file $out $default"
+echo $config
+python blf_genetic/application.py $config > $basename.log
+
+# profile 8
+basename="profile8"
+file="data/blf/$basename"
+out="-o $basename"
+config="$file $out $default"
+echo $config
+python blf_genetic/application.py $config > $basename.log
+
+# profile 6
+basename="profile6"
+file="data/blf/$basename"
+out="-o $basename"
+config="$file $out $default"
+echo $config
+python blf_genetic/application.py $config > $basename.log
+
+# profile 9
+basename="profile9"
+file="data/blf/$basename"
+out="-o $basename"
+config="$file $out $default"
+echo $config
+python blf_genetic/application.py $config > $basename.log
+
+#profile 10
+basename="profile10"
+file="data/blf/$basename"
+out="-o $basename"
+config="$file $out $default"
+echo $config
+python blf_genetic/application.py $config > $basename.log
+
